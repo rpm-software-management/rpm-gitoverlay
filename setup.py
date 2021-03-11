0d79bf80aedb7b55aa1cbd38e72f30145fd4a3a2
Daniel Mach <dmach@redhat.com>
2021-03-11 16:26:44 +0100

    Increase minimal Python version to 3.6

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ args = dict(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Software Development :: Build Tools",
         "Topic :: Utilities",
